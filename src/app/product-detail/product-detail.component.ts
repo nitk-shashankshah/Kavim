7471c04aa06f36b6d3878ecf51741079e8caaa79
Shashank Shah <shshah@extremenetworks.com>
2019-11-18 19:04:51 +0530

    slideshow

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -10,9 +10,36 @@ import { Location } from '@angular/common';
 })
 export class ProductDetailComponent implements OnInit {
 
-  id: string;
+  id: string;  
+  slideIndex:any = 1;
   private sub: any;
   constructor(private location: Location, private cartService:CartServiceService) {
+  }
+
+  
+  plusSlides(n) {
+    this.showSlides(this.slideIndex += n);
+  }
+  
+  currentSlide(n) {
+    this.showSlides(this.slideIndex = n);
+  }
+  
+  showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {this.slideIndex = 1}    
+    if (n < 1) {this.slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].setAttribute("style","display:none");  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    //slides[this.slideIndex-1].style.display = "block";  
+    slides[this.slideIndex-1].setAttribute("style","display:block");  
+    dots[this.slideIndex-1].className += " active";
   }
 
   ngOnInit() {
